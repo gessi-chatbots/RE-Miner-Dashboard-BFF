182f6ac70eb27a67fc7c4c0c028cc3a7dd0f61b9
mtiessler <max.tiessler@upc.edu>
2024-02-19 17:45:00 +0100

    dockerization

--- app/users_api/routes.py
+++ app/users_api/routes.py
@@ -1,6 +1,7 @@
 from . import users_api_bp
 from . import users_api_logger
 from datetime import datetime
+
 @users_api_bp.route("/register")
 def register_user():
     users_api_logger.info(f"[{datetime.now()}]Tryout") 
